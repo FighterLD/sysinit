e09682743c73a7366a2e2694543090ea82b3601c
liudong <liudong@cechealth.cn>
2020-07-06 14:53:39 +0800

    modify password

--- playbook/roles/initial/files/pswd.sh
+++ playbook/roles/initial/files/pswd.sh
@@ -4,7 +4,7 @@
 #echo "${newpsd}"|passwd --stdin root
 
 echo "ops@2019"|passwd --stdin ops
-echo "Cechealth@ops"|passwd --stdin ops
+echo "Ops@ops"|passwd --stdin ops
 echo "bed@2019"|passwd --stdin bed
 echo "bdd@2019"|passwd --stdin bdd
 echo "qad@2019"|passwd --stdin qad
